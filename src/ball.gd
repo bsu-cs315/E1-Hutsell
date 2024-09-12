5000f2f45abe557bb7330ae398025b205c523743
Paytonh13 <payton.hutsell@gmail.com>
2024-09-12 13:05:09 -0400

    Update ball.gd
    Changed the script to allow a better function of the ball moving. apply_impulse was changed to linear_velocity

--- src/ball.gd
+++ src/ball.gd
@@ -3,12 +3,18 @@ extends RigidBody2D
 @export var strength : float = 500
 
 
-
-func _process(delta):
+#func _process(_delta):
+#	if Input.is_action_just_pressed("launch"):
+#		var direction = Vector2(1, -1)
+#		var velocity = direction * strength
+#		apply_impulse(Vector2.ZERO, velocity)
+		
+		
+func _physics_process(_delta):
 	if Input.is_action_just_pressed("launch"):
-		#apply_impulse(Vector2(1000 , -1000))
-		var direction = Vector2(1, -1)
+		var direction = Vector2(1, -1).normalized()
 		var velocity = direction * strength
-		apply_impulse(Vector2.ZERO, velocity)
+		print("Velocity: ", velocity)
+		linear_velocity = velocity
 		
-		
+		#apply_impulse(Vector2(10, 10), velocity)
