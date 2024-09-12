26cdb22f08cd63ffac4d0c4cc66b0c0359f81a66
Paytonh13 <payton.hutsell@gmail.com>
2024-09-12 12:48:36 -0400

    Clean up Level.tscn

--- src/ball.gd
+++ src/ball.gd
@@ -3,17 +3,11 @@ extends RigidBody2D
 @export var strength : float = 500
 
 
-'func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("launch"):
-		var direction = Vector2(1, -1).normalized()
-		var velocity = direction * strength
-		apply_impulse(Vector2.ZERO, velocity)
-'
 
 func _process(delta):
 	if Input.is_action_just_pressed("launch"):
-		'apply_impulse(Vector2(1000 , -1000))'
-		var direction = Vector2(1, -1).normalized()
+		#apply_impulse(Vector2(1000 , -1000))
+		var direction = Vector2(1, -1)
 		var velocity = direction * strength
 		apply_impulse(Vector2.ZERO, velocity)
 		
